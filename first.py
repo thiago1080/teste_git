2aaae4922ffdf2d829b605cc6f45440f69a5c3ca
Thiago Henrique Martinelli <thiago.henrique.martinelli@gmail.com>
2019-05-24 13:57:19 -0300

    rm first

--- first.py
+++
@@ -1,8 +0,0 @@
-import json
-d = {}
-d['people'] = []
-d['people'].append({'Name':'Thiago', 'Surname':'Martinelli', 'sign':'Virgo'})
-d['people'].append({'Name':'Paul', 'Surname':'Simmons', 'sign':'Taurus'})
-
-with open('data.json', 'w') as f:
-	json.dump(d,f)
